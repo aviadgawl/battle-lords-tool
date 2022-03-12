7b77f29c8e48f997e3b7874d72b89b969165bbdd
aviad shua <aviadgawl@gmail.com>
2022-03-12 22:48:31 +0200

    Removing style rules from champion component

--- src/components/champion/champion.component.jsx
+++ src/components/champion/champion.component.jsx
@@ -7,18 +7,10 @@ export default function Champion(props) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.stat}>
         <Stat isReadOnly={props.isReadOnly} icon="str" startValue={0} title="STR:"></Stat>
-      </View>
-      <View style={[styles.stat, styles.dex]}>
         <Stat isReadOnly={props.isReadOnly} icon="dex" startValue={0} title="DEX:"></Stat>
-      </View>
-      <View style={[styles.stat, styles.int]}>
         <Stat isReadOnly={props.isReadOnly} icon="int" startValue={0} title="INT:"></Stat>
-      </View>
-      <View style={[styles.stat, styles.hp]}>
         <Stat isReadOnly={props.isReadOnly} icon="hp" startValue={0} title="HP:"></Stat>
-      </View>
     </View>
   );
 }
@@ -26,24 +18,8 @@ export default function Champion(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
     marginTop: 10,
     marginBottom: 10,
-  },
-  stat: {
-    flex: 1,
-    flexDirection: 'row',
-    marginLeft: 10,
-  },
-  dex: {
-    marginStart: 24,
-  },
-  int: {
-    marginEnd: 3,
-  },
-  hp: {
-    marginStart: 21,
+    width: '100%',
   },
 });
